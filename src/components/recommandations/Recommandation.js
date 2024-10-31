69d02471ab5be58b3e61efcf4da8ca6c05f4dd53
ClaireRuysschaert <netclaire@hotmail.fr>
2024-10-31 16:20:38 +0100

    Add recommandation

--- src/components/recommandations/Recommandation.js
+++ src/components/recommandations/Recommandation.js
@@ -169,6 +169,41 @@ const Recommandations = () => {
               </div>
             </div>
           </div>
+
+          {/* ================ Slider Three ================== */}
+
+                    <div className="w-full">
+            <div className="w-full h-auto flex flex-col lgl:flex-row justify-between">
+              <div className="w-full lgl:w-[35%] h-full bg-gradient-to-r from-[#1e2024] to-[#23272b] p-8 rounded-lg shadow-shadowOne flex flex-col md:flex-row lgl:flex-col gap-8 justify-center md:justify-start lgl:justify-center">
+                <img
+                  className="h-72 md:h-32 lgl:h-72 rounded-lg object-cover"
+                  src={yescapa}
+                  alt="yescapa"
+                />
+                <div className="w-full flex flex-col justify-end">
+                  <p className="text-xs uppercase text-designColor tracking-wide mb-2">
+                   Backend developer python/django
+                  </p>
+                  <h3 className="text-2xl font-bold">Fernando oksman de Aragao</h3>
+                  <p className="text-base tracking-wide text-gray-500">
+                    Collègue de travail (2 ans)
+                  </p>
+                </div>
+              </div>
+              <div className="w-full lgl:w-[60%] h-full flex flex-col text-justify">
+                <img className="w-20 lgl:w-32" src={quote} alt="quote" />
+                <br></br><br></br>
+                <div className="w-full h-[70%] py-10 bg-gradient-to-r from-[#1e2024] to-[#23272b] rounded-lg shadow-shadowOne p-4 lgl:p-8 flex flex-col justify-center gap-4 lgl:gap-8">
+                  <p className="text-base font-titleFont text-gray-400 font-medium tracking-wide leading-6">
+                  Claire is a great developer and work colleague. Always invested in learning more and participating. She is always available when needed.
+                  </p>
+                  <p className="text-gray-400">
+                    via <a href={linkedInLink} target="_blank" rel="noreferrer" ><u>LinkedIn</u></a> - Le 31 octobre 2024
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
         </Slider>
       </div>
     </section>
